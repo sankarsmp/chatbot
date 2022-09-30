055690cbf3237729740a1e0eae0794434ef95e1b
sankar <sankarsmp@gmail.com>
2022-09-30 09:05:16 +0530

    move al changes

---
+++ src/main/java/com/hackathon/chatbot/Chatbot.java
@@ -0,0 +1,22 @@
+package com.hackathon.chatbot;
+
+import org.alicebot.ab.Bot;
+import org.alicebot.ab.Chat;
+import org.springframework.stereotype.Component;
+@Component
+public class Chatbot {
+	Bot bot;
+	Chat chatSession;
+	public Chatbot() {
+		bot = new Bot("super", "");
+		chatSession = new Chat(bot);
+		bot.brain.nodeStats();
+	}
+
+	static String botName = "super";
+	
+	public String processRequest(String request) {
+		String response = chatSession.multisentenceRespond(request);
+		return response;
+	}
+}
--- src/main/java/com/hackathon/chatbot/controller/CommandController.java
+++ src/main/java/com/hackathon/chatbot/controller/CommandController.java
@@ -1,21 +1,27 @@
 package com.hackathon.chatbot.controller;
 
-import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hackathon.chatbot.Chatbot;
+
 @RestController
 @RequestMapping("/command")
 public class CommandController {
 	Logger logger = LogManager.getLogger(CommandController.class);
+	@Autowired
+	Chatbot chatbot;
 	@PostMapping("/service")
     public String commandProcessor(@RequestParam("commandInput") String commandInput) {
-        System.out.println("commandInput:"+commandInput);
         logger.info("logcommandInput:"+commandInput);
-		return "Hello, World!";
+        String response = chatbot.processRequest(commandInput);
+        logger.info("response:"+response);
+		return response;
     }
 
 }
